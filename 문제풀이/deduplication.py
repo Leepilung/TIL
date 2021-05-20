1edc3a42c9db9aff21f5755e52f4122c474d5a56
Lee Pil Ung <ek3434@naver.com>
2021-05-20 13:24:04 +0900

    반복 삭제

--- 문제풀이/deduplication.py
+++ 문제풀이/deduplication.py
@@ -1,12 +1,23 @@
 #주어진 배열에서 반복되는 숫자를 전부 제거하는 함수를 만들어보세요.
-#[1, 1, 2, 2, 4, 4, 4, 1] -> [1, 2, 4, 1]
+#A[start] == A[i] -> len(A)
+#start = 0 -> start < len(A)
+# start = 0 A =[1,2,2,4,4,4,1] -> start = 0 LEN(A) = 7 start = 0 < len 6
+# start = 0 A[0] != A[1] -> else -> start += 1 start =1 
+# start = 1 A[1] == A[2] -> DLEA[2] A = [1,2,4,4,4,1] - start = 1 LEN(A) =6 start = 1 < len 5
+# start = 1 A[1] != A[2] -> else -> start +=1 start =2
+# start = 2 A[2] == A[3] -> DLE A[3] A = [1,2,4,4,1] -> start = 2 LEN(A) = 5 start = 2 < len 4
+# start = 2 A[2] == A[3] -> DLE A[3] A = [1,2,4,1] -> start = 2 LEN(A) = 4 start = 2 < len 3
+# start = 2 A[2] != A[3] -> else -> start +=1 start =3 start 3 <= len(3)
+# start = 3 A[3] != A[4] -> else -> start +=1 start =4
+A = [1, 1, 2, 2, 4, 4, 4, 1]
 
-list = list(map(int, input('리스트를 입력하시오. ex) 1 2 3 4 : ')))
-#map, split 쓰면 연달아 입력할 수 있음. 이를 int로 숫자형 ->list로 리스트형으로 변환
+def duplication(A):
+    start = 0
+    while start < len(A)-1:
+        if A[start] == A[start +1]:
+            del A[start+1]
+        else:
+            start += 1
+    return len(A)
 
-def deduplication(num):
-    numset = set(num) #중복제거는 set함수 이용.
-    print(numset)
-
-deduplication(list)
-
+print(duplication(A))
