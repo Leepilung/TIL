6d71a68d5e207d5c299f14d41382a2c25bc8b65f
Lee Pil Ung <ek3434@naver.com>
2021-05-20 13:23:41 +0900

    조건부배열

--- 문제풀이/conditionalarray.py
+++ 문제풀이/conditionalarray.py
@@ -1,7 +1,7 @@
 #주어진 배열에서 홀수는 모두 오른쪽, 짝수는 모두 왼쪽으로 정렬하는 함수를 만들어보세요.
 # [1, 2, 3, 4] -> [2, 4, 1, 3]
 
-def conditionalarray(self, A):
+def conditionalarray(A):
         add1, add2 = [], []
         for i in A:
             if i % 2 == 0:
@@ -10,5 +10,5 @@ def conditionalarray(self, A):
                 add2.append(i)
         return add1+add2
 
-a = conditionalarray((),[1,2,3,4])
+a = conditionalarray([1,2,3,4])
 print(a)
