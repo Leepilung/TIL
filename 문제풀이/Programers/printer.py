1afc4b4cd120ca0f641af593f9b9b9ffe4b04b3d
Lee Pil Ung <ek3434@naver.com>
2021-05-24 16:28:03 +0900

    프린터 알고리즘

---
+++ 문제풀이/Programers/printer.py
@@ -0,0 +1,25 @@
+#priorities = [2,1,3,2], 중요도는 C D A B 즉 3,2,2,1 순으로 입력. 
+#location = 2일 경우 3이 몇번째로 출력되는지 알고싶은 문제. 저 문제에선 가장 큼으로 return = 1
+#location은 인덱스.
+
+def solution(priorities, location):
+    answer = 0
+    maxpir = priorities.copy()
+    maxpir.sort()
+
+    while len(priorities) != 0:
+        if priorities[0] == maxpir(-1):
+            answer += 1
+            priorities.pop(0)
+            if location == 0:
+                return answer
+            else:
+                location -= 1
+                
+        else:
+            priorities.append(priorities.pop(0))
+            if location = 0:
+                location = len(priorities) -1
+            else: 
+                location -= 1
+    return answer
