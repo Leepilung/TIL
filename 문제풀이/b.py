f54601019c2efb9842c5fb4a733d8553741547bd
Lee Pil Ung <ek3434@naver.com>
2021-05-24 18:52:56 +0900

    기능개발 풀이용 연습시트

--- 문제풀이/b.py
+++ 문제풀이/b.py
@@ -1,20 +1,39 @@
-#보석 돌 풀이+실험용 시트
+p = [93, 30 , 55]
+s = [1, 30, 5]
+answer = []
+timegab = []
+count = 1
+import math
+for i,j in zip(p,s):    
+    timegab.append(math.ceil((100 - i) / j))
+    if timegab[0] > timegab[1]:
+        timegab.pop(1)
+        count +=1
+    elif timegab[0] < timegab[1]:
+        timegab.pop(0)
+        answer.append(count)
+    else:
+        timegab.pop(0)
+        timegab.pop(1)
+        count+=2
 
-jewels = 'aA'
-stone = 'aAAbbbbb'
-sepjewels = list(jewels)
-print(sepjewels)
-# -> a와 A가 남음.
-for j in stone:
-#인덱스로 표시하기때문에     
-#결과값 print(j)하면a, A, A, b,b,b,b,b로 나옴
-#위 부분은 함수에서 바꾸면 j for j in stone 이 꼴로 변환 가능.
-#그렇다면 저 반복문 sepjewels in j 꼴 쓰려면?
-#이중 for문은 시간복잡도가 커져서 사용 X.
-#결과값 예상대로 나옴 (for j in stone = ㅁ,)
+print(timegab) -> [7,3,9]
+print(timegab[0]) -> 7
+print(timegab[1]) -> 3
+timegab.pop(0) -> 7 pop으로 삭제
+print(timegab) -> [3,9]
+# 7, 3, 9 출력 -> return [2,1]
+# 7 계산,출력하고 count에 1가산, 3계산하고 count에 또 1가산. 여기서 조건문 들어가야함
+# answer[0] > answer[1] 일때만 반복. 만약 answer[0] < answer[1]이면 별도로
+#배포가 진행된다. answer[0]
+print('----------------')
 
-#j in sepjewels for j in stone #가능한가? -> 확인 불가능
+p2 = [95, 90, 99, 99, 80, 99]
+s2 = [1, 1, 1, 1, 1, 1]
+for i,j in zip(p2,s2):    
+    done2 = math.ceil((100 - i) / j)
+    print(done2)
+    
+# 5, 10, 1, 1, 20, 1 출력 -> return[1,3,2]
 
-#변수 하나를 초기값 0으로 설정.
-#a A A b b b b b가 sepjewels에 있을경우 아무 변수 1씩 증가시키고
-
+# 1, 1, 3 , 1, 2, 10, 5 -> return[2,3,2]
