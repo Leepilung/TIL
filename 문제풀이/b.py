75cbd49580c28bdb2337cf0d5cf5c4b958958ca9
Lee Pil Ung <ek3434@naver.com>
2021-05-26 13:14:47 +0900

    트럭 알고리즘 연습용 파일

--- 문제풀이/b.py
+++ 문제풀이/b.py
@@ -1,14 +1,21 @@
-a = [1,2,3,4,5]
-answer = [1,2,3,5,4]
+for i in range(7):
+    for j in range(8):
+        print(f'{i}  ',end ='')
+        print(f'{j}  ',end = '')
+        if j == 7:
+            print()
 
-b =[0,5,4]
-b.sort()
-print(b[-1])
+print('-----------------------------------------------')
 
+weight = 10
+bridge_lenth = 2
+truck_weights = [7,4,5,6]
+total = 0
+bridge= [0]*bridge_lenth
 
-answer 1,2,3,4,5
-math1 = 1,2,3,4,5 
-math1%5 = 1,2,3,4,0
-
-answer[0]
-
+temp2 = truck_weights.pop(0)
+print(temp2)
+total += temp2
+print(total)
+bridge.append(truck_weights.pop(0))
+print(bridge)
