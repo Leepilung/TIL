b685e1c313a854ac7a567cc2d4ab65c174c51ff9
Lee Pil Ung <ek3434@naver.com>
2021-05-29 11:08:16 +0900

    counting bits 풀이용 파일

--- 문제풀이/test.py
+++ 문제풀이/test.py
@@ -1,35 +1,9 @@
-p = [95, 90, 99, 99, 80, 99]
-s = [1, 1, 1, 1, 1, 1]
-answer = []
-timegab = []
-count = 1
-import math
-for i,j in zip(p,s):    
-    timegab.append(math.ceil((100 - i) / j))
-    while len(timegab) >= 2 :
-        if timegab[0] < timegab[1]:
-            timegab.pop(0)
-            answer.append(count)
-            count = 1
-        
-        elif timegab[0] >= timegab[1]:
-            timegab.pop(1)
-            count +=1
-if len(timegab) == 1:
-    answer.append(count)
+# bin(int(n)) -> 입력값 정수형 n을 2진수로 변환하는 내장함수. 
+# 안에 1이 몇개있는지 확인하는건 count
+a = 2
+b = []
 
-print(count)
-print(answer)
-print(timegab)
-print(count)
+for i in range(a+1):
+    b.append(bin(i).count('1'))
 
-
-## 7 3 9
-#
-#
-#
-#
-#
-#
-#
-#
+print(b)
