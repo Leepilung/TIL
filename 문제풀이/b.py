fa505ae115cd6b4ae34be8d5835916dcca909b85
Lee Pil Ung <ek3434@naver.com>
2021-06-06 23:44:40 +0900

    신규아이디추천 알고리즘 풀이용 파일

--- 문제풀이/b.py
+++ 문제풀이/b.py
@@ -1,10 +1,47 @@
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+new_id = "=.="
+answer = ''
+step2 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','0','-','_','.']
+#step 1
+new_id = new_id.lower()
+print(new_id)
 
-for i in range(len(record)):
-    a = record[i].split()
-    if a[0] == 'Enter':
-        print(f"{a[2]}님 이 들어왔습니다.")
-    if a[0] == 'Leave':
-        print(f"{a[1]}님이 나갔습니다.")
-    print(a)
-    
+#setp 2
+for i in new_id:
+    if i in step2:
+        answer += i
+new_id = answer
+print('step2',answer)
+
+#step 3
+while '..' in answer:
+    answer = answer.replace('..','.')
+print('step3',answer)
+
+#step 4
+if answer[0] == '.':
+    if len(answer) > 1:
+        answer = answer[1:]
+    else:
+        '.'
+if answer[-1] == '.':
+    answer = answer[:-1]
+print('step4',answer)
+
+#step 5
+if len(answer) == 0:
+    answer = answer + 'a'
+print('step5',answer)
+#step 6 
+if len(answer) >= 16:
+    answer = answer[:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+print('setp6',answer)
+#step 7
+if len(answer) <= 2:
+    while len(answer) < 3:
+        answer = answer + answer[-1]
+print('step7',answer)
+
+print('-----')
+print(answer)
