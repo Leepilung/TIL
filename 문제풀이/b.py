2bd2d0ecbb6fd1527ea60933fb8cb06be3770bfd
Lee Pil Ung <ek3434@naver.com>
2021-06-07 14:00:00 +0900

    더 맵게 알고리즘 연습용 파일

--- 문제풀이/b.py
+++ 문제풀이/b.py
@@ -1,47 +1,28 @@
-new_id = "=.="
-answer = ''
-step2 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','0','-','_','.']
-#step 1
-new_id = new_id.lower()
-print(new_id)
+# 스택 사용 -> 대부분 통과하나 일부 런타임 에러 실패. + 효율성 통과 실패.
+# heap 사용. heapqpop -> 작은값을 알아서 빼줌. 
+# heappush -> 인수로 받은값 알아서 최소로 정렬하고 넣음.
+# 
 
-#setp 2
-for i in new_id:
-    if i in step2:
-        answer += i
-new_id = answer
-print('step2',answer)
+import heapq
 
-#step 3
-while '..' in answer:
-    answer = answer.replace('..','.')
-print('step3',answer)
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+answer = 0
+heap = []
+for i in scoville:
+    heapq.heappush(heap,i)
+print('heap =',heap)
 
-#step 4
-if answer[0] == '.':
-    if len(answer) > 1:
-        answer = answer[1:]
-    else:
-        '.'
-if answer[-1] == '.':
-    answer = answer[:-1]
-print('step4',answer)
-
-#step 5
-if len(answer) == 0:
-    answer = answer + 'a'
-print('step5',answer)
-#step 6 
-if len(answer) >= 16:
-    answer = answer[:15]
-    if answer[-1] == '.':
-        answer = answer[:-1]
-print('setp6',answer)
-#step 7
-if len(answer) <= 2:
-    while len(answer) < 3:
-        answer = answer + answer[-1]
-print('step7',answer)
-
-print('-----')
+while heap[0] <= K:
+    bucket = heapq.heappop(heap) + (heapq.heappop(heap)*2)
+    heapq.heappush(heap,bucket)
+    answer +=1
+    print(bucket)
+    print(heap)
+    if heap[0] <= K and len(heap) <= 1:
+        answer = -1
+        break
+    if heap[0] >= K:
+        break
+print(heap)
 print(answer)
