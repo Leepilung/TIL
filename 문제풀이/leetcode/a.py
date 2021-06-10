481c0c4d9367820e7b869db52e4fc0e9a0590c1d
Lee Pil Ung <ek3434@naver.com>
2021-06-10 13:04:42 +0900

    가장먼 노드 알고리즘 deque 사용시

--- 문제풀이/leetcode/a.py
+++ 문제풀이/leetcode/a.py
@@ -1,20 +1,25 @@
-path = "/a/./b/../../c/"
-a = path.split('/')
-level = 0
-stack = []
+s1 = "aabcc" 
+s2 = "dbbca" 
+s3 = "aadbbcbcac"
 
-print(a)
-print('--------------------')
-for i in a:
-    if i != '' and i not in ['.','..']:
-      level +=1
-      stack.append('/'+ i)
-    if i == '..':
-        level -= 1
-        if len(stack) != 0:
-            stack.pop(0)
 
-if len(stack) == 0:
-    stack.append('/')
-
-print(''.join(stack))   
+#s1[0] == s3[0] == -> 재탐색
+#s1[1] == s3[1] -> 재탐색
+#s1[2] != s3[2] -> s2에서 찾기.
+#s2[0] == s3[2] -> s2에서 재탐색
+i1 = 0
+i2 = 0
+i3 = 0
+while i1 == len(s1) or i2 == len(s2):
+    if s1[i1] == s3[i3]:
+        i1 += 1
+        i3 += 1
+    elif s2[i2] == s3[i3]:
+        i2 +=1
+        i3 +=1
+    else: # s3[i3] != s1[i1] and s3[i3] != s2[i2]:
+        print(False)
+        break
+    if i3 == len(s3):
+        print(True)
+        break
