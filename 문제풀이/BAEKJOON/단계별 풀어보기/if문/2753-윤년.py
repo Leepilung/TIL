9283edc9fed1825e437018d616242ee31b815295
Lee Pil Ung <ek3434@naver.com>
2021-07-01 12:13:32 +0900

    TIL - 알고리즘 복습해야 할 문제들

---
+++ 문제풀이/BAEKJOON/단계별 풀어보기/if문/2753-윤년.py
@@ -0,0 +1,24 @@
+# 백준 알고리즘 - if문 - 2753. 윤년
+# https://www.acmicpc.net/problem/2753
+# 연도가 주어졌을 때, 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오
+# 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+
+# 아주 간단한 문제. 바로 통과못했던 간과한점 ->  조건문을 2중으로 썼으나 else문을 oddYear % 4 == 0:일때만 넣었음.
+# if문에 else문 여러번 사용하도록 하는것 주의하기.
+# 입력부분을 굳이 범위 제한 루프문써서 구현 안해도 통과됨. -> 문제있지않나 ㅋㅋ
+
+
+while True:
+    oddYear = int(input())
+    if 4000 >= oddYear >= 1:
+        break
+    else:
+        continue
+    
+if oddYear % 4 == 0:
+    if oddYear % 100 != 0 or oddYear % 400 == 0:
+        print(1)
+    else:
+        print(0)
+else:
+    print(0)
