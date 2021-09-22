dd9e0065f2add2acacd31419eab7b9cfe37b269b
Lee Pil Ung <ek3434@naver.com>
2021-09-22 23:42:22 +0900

    TIL - 정글 0922

---
+++ 일반/SWJungle/실습/Chapter3/prac_python/hello.py
@@ -0,0 +1,8 @@
+people = [{'name': 'bob', 'age': 20}, 
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
+
+for i in people:
+    print(i['age'])
