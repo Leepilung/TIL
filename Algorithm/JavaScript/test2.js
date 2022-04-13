86553c9b3a5643ee6d9be96a51e95e1d9d8eadd3
Lenol <ek3434@naver.com>
2022-04-13 23:52:30 +0900

    JS 문법 - replace()

--- Algorithm/JavaScript/test2.js
+++ Algorithm/JavaScript/test2.js
@@ -1,22 +1,6 @@
-function solution(fetchExperts, fetchIsExpertOnline) {
-    // 보험 전문가의 목록을 반환하는 비동기 함수 (반환 타입: Promise<string[]>)
-    fetchExperts();
-    
-    // 보험 전문가가 온라인인지 여부를 반환하는 함수 (반환 타입: Promise<boolean>)
-    fetchIsExpertOnline('배수진');
-    
-    return [];
-}
+var comb = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"];
 
-
-function solution(type, id, listener) {
-    return {
-        type,
-        id,
-        onEvent(event) {
-            listener(event);
-        },
-        addChild(node) {},
-        removeChild(node) {},
-    }
-}w
+console.log(comb.includes("ABC"));
+console.log("ABC" in comb);
+console.log();
+console.log();
