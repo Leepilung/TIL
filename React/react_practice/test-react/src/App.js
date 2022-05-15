63faab86c87f56b3af1e1f741cdd96e53a19bbae
Lenol <ek3434@naver.com>
2022-05-15 23:58:24 +0900

    props 예제

--- React/react_practice/test-react/src/App.js
+++ React/react_practice/test-react/src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
+import MyComponent from "./Components/MyComponents";
 
-function App() {
-    const name = "리액트";
-    return (
-        <>
-            {/* 주석은 이렇게 작성합니다. 
-            이건? 이것도되네 ㅋㅋ*/}
-            <div
-                className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있습니다.
-            >
-                {name}
-            </div>
-            // 하지만 이런 주석이나 / 이런 주석은 페이지에 그대로 나타나게
-            됩니다. */
-            <input />
-        </>
-    );
-}
+const App = () => {
+    return <MyComponent name="이필웅" />;
+};
 
 export default App;
