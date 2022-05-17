b63b776961e80cc80f6ea9cab789da5f59fe8e59
Lenol <ek3434@naver.com>
2022-05-17 22:01:22 +0900

    React - state, 예제

--- React/react_practice/test-react/src/App.js
+++ React/react_practice/test-react/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Counter from "./Components/Counter";
-
+import Say from "./Components/Say";
+import EventPractice from "./Components/ClassEventPractice";
+import FuncEventPractice from "./Components/FuncEventPractice";
 const App = () => {
-  return <>
-  <Counter />;
-  안녕하세요;
-  </>
+    return (
+        <>
+            <Counter />; 안녕하세요;
+            <hr />
+            <Say />
+            <hr />
+            <EventPractice />
+            <hr />
+            <FuncEventPractice />
+        </>
+    );
 };
 
 export default App;
