513fd6b20498450a51986b23e2cd7046f45aa170
Lenol <ek3434@naver.com>
2022-05-19 12:36:03 +0900

    React Styling 컴포넌트 예제

---
+++ React/react_practice/styling-react/src/App.js
@@ -0,0 +1,10 @@
+import SassComponent from "./Components/SassComponent";
+function App() {
+    return (
+        <div>
+            <SassComponent />
+        </div>
+    );
+}
+
+export default App;
