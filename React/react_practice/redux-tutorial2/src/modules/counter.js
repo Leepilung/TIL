fa07db65f1b6ac9ded5f2edcd112ead05bf58cc3
Lenol <ek3434@naver.com>
2022-05-27 01:19:19 +0900

    redux-action 사용

--- React/react_practice/redux-tutorial2/src/modules/counter.js
+++ React/react_practice/redux-tutorial2/src/modules/counter.js
@@ -1,10 +1,16 @@
+import { createAction, handleActions } from "redux-actions";
+
 // 액션 정의
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+
+//redux-actions 깔면 아래처럼 바뀜 {createAction} import 하면됨
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태
 const initialState = {
@@ -12,19 +18,29 @@ const initialState = {
 };
 
 // 리듀서
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1,
-            };
-        case DECREASE:
-            return {
-                number: state.number - 1,
-            };
-        default:
-            return state;
-    }
-}
+
+// function counter(state = initialState, action) {
+//     switch (action.type) {
+//         case INCREASE:
+//             return {
+//                 number: state.number + 1,
+//             };
+//         case DECREASE:
+//             return {
+//                 number: state.number - 1,
+//             };
+//         default:
+//             return state;
+//     }
+// }
+
+// { handleActions} import 하면 아래처럼됨.
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+    },
+    initialState,
+);
 
 export default counter;
