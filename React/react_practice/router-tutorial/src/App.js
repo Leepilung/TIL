7ed42e6fdb645a70e1ad51506a4fd777ac45a1e9
Lenol <ek3434@naver.com>
2022-06-11 22:40:35 +0900

    RRD - 다중 path

--- React/react_practice/router-tutorial/src/App.js
+++ React/react_practice/router-tutorial/src/App.js
@@ -2,10 +2,13 @@ import { Route, Routes, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Profiles from "./Components/Profiles";
+import NavigateSample from "./Components/NavigateSample";
 
+// React-route v6 기준
 // Route 사용할거라면 <Routes>로 감싸줘야함
 // component -> element로 바꿔서 사용해야함.
 // 컴포넌트도 <컴포넌트이름/>의 형태
+// v5에서 배열로 여러개의 패스를 지정하는 방식은 사용할 수 없음. 그냥 그 이전에 사용하던 대로 여러개의 패스를 만들어야 함.
 
 const App = () => {
     return (
@@ -20,12 +23,16 @@ const App = () => {
                 <li>
                     <Link to="/profiles">프로필</Link>
                 </li>
+                <li>
+                    <Link to="history">History 예제</Link>
+                </li>
             </ul>
             <hr />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/profiles/*" element={<Profiles />} />
+                <Route path="/history" element={<NavigateSample />} />
             </Routes>
         </>
     );
