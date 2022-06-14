5a3ce93409dc4b288ec3d8f8b68bcfa44c5a8dec
Lenol <ek3434@naver.com>
2022-06-14 20:48:03 +0900

    ContextAPI - useContext 최적화

--- React/react_practice/context-tutorial/src/Components/ColorBox.js
+++ React/react_practice/context-tutorial/src/Components/ColorBox.js
@@ -1,28 +1,30 @@
-import React from "react";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext, { ColorConsumer } from "../contexts/color";
+
+// useContext를 활용하여 코드 간결화 가능.
 
 const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+
     return (
-        <ColorConsumer>
-            {({ state }) => (
-                <>
-                    <div
-                        style={{
-                            width: "64px",
-                            height: "64px",
-                            background: state.color,
-                        }}
-                    />
-                    <div
-                        style={{
-                            width: "32px",
-                            height: "32px",
-                            background: state.subcolor,
-                        }}
-                    />
-                </>
-            )}
-        </ColorConsumer>
+        <>
+            <>
+                <div
+                    style={{
+                        width: "64px",
+                        height: "64px",
+                        background: state.color,
+                    }}
+                />
+                <div
+                    style={{
+                        width: "32px",
+                        height: "32px",
+                        background: state.subcolor,
+                    }}
+                />
+            </>
+        </>
     );
 };
 
