83edf65c9fde48ea9da0d447a26dd262d2ad5fe5
Lenol <ek3434@naver.com>
2022-06-16 12:36:34 +0900

    SSR - Rendering

--- Error/Mac.md
+++ Error/Mac.md
@@ -8,3 +8,12 @@
 # brew 코어 디렉토리 삭제 명령어
 $ rm -fr $(brew --repo homebrew/core)
 ```
+
+# 맥북 5000 포트 점유(address already in use :::5000)
+
+macOS Monterey 이상으로 업데이트 했을 시 AirPlay 모드 기능이 켜져있으면 5000번 포트를 점유하고 있음.
+
+lsof ~ kill 명령어로는 해결할 수 없고
+
+
+시스템 환경 설정 - 공유 -> AirPlay 수신 모드 항목의 체크를 해제하면 된다.
--- React/react_practice/ssr-recipe/src/index.server.js
+++ React/react_practice/ssr-recipe/src/index.server.js
@@ -1,8 +1,79 @@
 import React from "react";
 import ReactDOMServer from "react-dom/server";
+import express from "express";
+// StaticRouter의 경우 v6로 오면서 import 경로가 바뀜
+import { StaticRouter } from "react-router-dom/server";
+import App from "./App";
+// 정적 파일 접근 코드
+import path from "path";
 
-const html = ReactDOMServer.renderToString(
-    <div>Hello Server Side Rendering!</div>,
+import fs from "fs";
+// asset-manifest.json에서 파일 경로들을 조회하는 구문
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve("./build/asset-manifest.json"), "utf8"),
 );
 
-console.log(html);
+const chunks = Object.keys(manifest.files)
+    .filter((key) => /chunk\.js$/.exec(key)) // chunk.js로 끝나는 키를 찾는 구문
+    .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환 하는 구문
+    .join(""); // 위의 두 구문의 결과물을 합친다.
+
+function createPage(root) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta 
+            name="viewport"
+            content="width=device=width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <title>Reacvt App</title>
+            <link hre="${manifest.files["main.css"]}" rel="stylesheet" />
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app </noscript>
+        <div id="root">
+            ${root}
+        </div>
+        <script src="${manifest.files["runtime~main.js"]}"></script>
+        ${chunks}
+        <script src="${manifest.files["main.js"]}"></script>
+    </body>
+    </html>
+    `;
+}
+
+const app = express();
+
+// 서버 사이드 렌더링 처리용 핸들러 함수
+const serverRender = (req, res, next) => {
+    // 이 함수는 404가 떠야 하는 상황에 404를 안띄우고 서버 사이드 렌더링을 하게 해주는 역할
+
+    const context = {};
+    const jsx = (
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링 구문
+    res.send(createPage(root)); // 클라이언트에 결과물 응답으로 보냄
+};
+
+const serve = express.static(path.resolve("./build"), {
+    index: false, // “/” 경로에서 index.html을 보여 주지 않도록 설정
+});
+
+app.use(serve); // 순서에 유의해야한다. serverRender 이전에 위치해야 한다.
+app.use(serverRender);
+
+app.listen(5000, () => {
+    console.log("Running on http://localhost:5000");
+});
+
+// const html = ReactDOMServer.renderToString(
+//     <div>Hello Server Side Rendering!</div>,
+// );
+
+// console.log(html);
