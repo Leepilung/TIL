4edc1f26f750a2baee8fd8db8a81391e7295c231
Leepilung <ek3434@naver.com>
2022-07-11 19:26:41 +0900

    Vue 튜토리얼

---
+++ Vue/composition_api.vue
@@ -0,0 +1,23 @@
+<!-- 컴포지션 API -->
+<!-- import해서 가져온 API 함수들을 이용하여 로직을 정의한다. -->
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// 반응적인 상태의 속성
+const count = ref(0)
+
+// 속성 값을 변경하고 업데이트 할 수 있는 함수.
+function increment() {
+  count.value++
+}
+
+// 생명 주기 훅
+onMounted(() => {
+  console.log(`숫자 세기의 초기값은 ${count.value} 입니다.`)
+})
+</script>
+
+<template>
+  <button @click="increment">숫자 세기: {{ count }}</button>
+</template>
--- Vue/practice/vue-practice/src/main.js
+++ Vue/practice/vue-practice/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// 최상위 컴포넌트 옵션
 const app = createApp(App);
 
 app.use(router);
 
+// 앱 인스턴스는 mount()가 호출되기 전까지 아무 것도 렌더링 하지 않는다.
 app.mount("#app");
