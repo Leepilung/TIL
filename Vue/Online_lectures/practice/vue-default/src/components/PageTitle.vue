e2fb505a2545d2e5aca041452ca4fc1b3c9323d5
Leepilung <ek3434@naver.com>
2022-07-22 11:54:14 +0900

    Vue : 컴포넌트 삽입

---
+++ Vue/Online_lectures/practice/vue-default/src/components/PageTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 class="page-title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  // Vue에서 자식 컴포넌트에서 props를 사용하는 방법
+  props: {
+    title: {
+      type: String,
+      default: '페이지 타이틀',
+    },
+  },
+};
+</script>
+
+<style scoped>
+.page-title {
+  padding-left: 20px;
+  border-left: 5px solid red;
+  text-align: left;
+}
+</style>
--- Vue/Online_lectures/practice/vue-default/src/views/ServerData.vue
+++ Vue/Online_lectures/practice/vue-default/src/views/ServerData.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div>
+      <!-- 컴포넌트 삽입 -->
+      <PageTitle title="부모 컴포넌트에서 보낸 제목" />
+    </div>
     <button type="button" @click="getProductList">데이터 조회하기</button>
     <table>
       <thead>
@@ -21,7 +25,12 @@
 </template>
 <script>
 import axios from 'axios';
+// 컴포넌트 삽입 구문
+import PageTitle from '../components/PageTitle';
+
 export default {
+  name: 'NaNi',
+  components: { PageTitle },
   data() {
     return {
       productList: [],
