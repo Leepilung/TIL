330b914280da89a3e7b2f7d8596f96944d0527dd
Leepilung <ek3434@naver.com>
2022-07-28 10:54:13 +0900

    Vue: Front단 페이지 및 컴포넌트

--- Vue/practice/login-practice/src/router/index.js
+++ Vue/practice/login-practice/src/router/index.js
@@ -1,20 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import DashBoard from '../views/DashBoard.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'HhomeView',
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/dashboard',
+    name: 'DashBoard',
+    component: DashBoard,
+  },
+  {
+    path: '/:PathMatch(.*)',
+    name: 'not-found',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+      import(/* webpackChunkName: "about" */ '../views/ErrorView.vue'),
   },
 ];
 
